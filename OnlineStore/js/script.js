0fca24561d93dd5a2ca06b2fe591aa58150f7c5c
Nik6996 <nik.taraskov@ukr.net>
2021-05-04 20:54:51 +0400

    validation

--- OnlineStore/js/script.js
+++ OnlineStore/js/script.js
@@ -77,6 +77,7 @@ new Swiper('.slider-extra', {
 	}
 
 })
+
 document.addEventListener('DOMContentLoaded', function () {
 	const form = document.getElementById('form');
 	form.addEventListener('submit', formSend);
@@ -86,20 +87,51 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		let error = formValidate(form);
 
+
+		// let formData = new FormData(form);
+		// formData.append();
+
+		if (error === 0) {
+
+			alert("все ок");
+			form.classList.add('_sending');
+			let response = await fetch('sendmail.php', {
+				method: 'POST',
+				body: formData
+			});
+			if (response.ok) {
+				let response = await response.json();
+				alert(result.message);
+				form.reset();
+				form.classList.remove('_sending');
+			} else {
+				alert('ошибка отправки формы')
+				form.classList.remove('_sending');
+			}
+
+		} else {
+			alert('заполните форму');
+		}
+
+
 	}
 	function formValidate(form) {
 		let error = 0;
 		let formReq = document.querySelectorAll('._req');
 
+
 		for (let index = 0; index < formReq.length; index++) {
 			const input = formReq[index];
 			formRemoveError(input);
+
 
 			if (input.classList.contains('_email')) {
 				if (emailTest(input)) {
 					formAddError(input);
 					error++;
+
 				}
+
 
 			} else if (input.classList.contains('_phone')) {
 				if (phoneTest(input)) {
@@ -116,7 +148,9 @@ document.addEventListener('DOMContentLoaded', function () {
 				}
 			}
 		}
+		return error;
 	}
+
 	function formAddError(input) {
 		input.parentElement.classList.add('_error');
 		input.classList.add('_error');
@@ -133,6 +167,66 @@ function phoneTest(input) {
 	return !/^(?:\(\d{3}\)|\d{3})(?: *- *)?\d{3}(?: *- *)?\d{4}$/.test(input.value);
 }
 
+//-----------------------------------валидация регистрации----------------------------------//
 
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('login');
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		e.preventDefault();
+		let error = formValidate(form);
+		if (error === 0) {
+			alert("все ок");
+			form.classList.add('_sending');
+			let response = await fetch('sendmail.php', {
+				method: 'POST',
+				body: formData
+			});
+			if (response.ok) {
+				let response = await response.json();
+				alert(result.message);
+				form.reset();
+				form.classList.remove('_sending');
+			} else {
+				alert('ошибка отправки формы')
+				form.classList.remove('_sending');
+			}
+		}
+	}
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._require');
+
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input);
+
+			if (input.classList.contains('_phone')) {
+				if (phoneTest(input)) {
+					formAddError(input);
+					error++
+				}
+			} else {
+				if (input.value === '') {
+					formAddError(input);
+					error++;
+				}
+			}
+		}
+		return error;
+	}
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+});
+function phoneTest(input) {
+	return !/^(?:\(\d{3}\)|\d{3})(?: *- *)?\d{3}(?: *- *)?\d{4}$/.test(input.value);
+}
 
 // const { use } = require("browser-sync");
